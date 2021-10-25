1573bca9cae921cd43c09bafbd90b06c458b8a1e
Stefanos Markos <smarkos101@gmail.com>
2021-10-25 20:35:06 +0300

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GighubMVCFixed.Startup))]
+namespace GighubMVCFixed
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
